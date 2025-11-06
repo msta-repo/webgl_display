aac0e205b07fce98d20a4fc2fe289cebb8810903
mmmmsta <michael.st1000@gmail.com>
2025-11-06 17:44:40 +0100

    fix particle rendering issue
    only use canvas size on display step

--- display_composite.glsl
+++ display_composite.glsl
@@ -24,7 +24,7 @@ void main() {
     vec3 color;
     color = vec3(0.05,0.15,0.9);
 
-    float density_factor = squeeze( FC.z ,0.8,  1.2) - 0.5;
+    float density_factor = squeeze( FC.z ,0.8,  2.0);
 
     float velocity_factor = squeeze( abs(FC.y),0.0, 0.1);
 
@@ -32,7 +32,7 @@ void main() {
     vec3 fluid = vec3(0.5, 0.0,0.7)*density_factor;
 
     // Composite trail on top of fluid
-    vec3 finalColor = clamp(fluid.rgb +  trail.rgb, 0.0,1.0);
+    vec3 finalColor = fluid.rgb+  trail.rgb;
     
     //gl_FragColor = vec4(finalColor, 1.0);
 
