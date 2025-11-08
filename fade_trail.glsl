4678c39e1ce69204bfeb9d78e12f6221c1d47dcb
mmmmsta <michael.st1000@gmail.com>
2025-11-08 12:33:43 +0100

    improve trail and add simulation scale parameter

--- fade_trail.glsl
+++ fade_trail.glsl
@@ -4,10 +4,16 @@ uniform sampler2D previousTrail;
 uniform vec2 resolution;
 uniform float fadeAmount;  // 0.0 = instant fade, 1.0 = no fade
 
+
+
+
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution;
     vec4 prevColor = texture2D(previousTrail, uv);
     
+    //float fadeSmooth = erf_fast((fadeAmount-5.0)/2.0);
+
+
     // Fade the trail
-    gl_FragColor = prevColor * fadeAmount;
+    gl_FragColor = prevColor *fadeAmount/10.0;
 }
